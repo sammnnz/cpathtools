a66684df08a88bbe2e3bc81774451b65b52fcb70
sammnnz <104736093+sammnnz@users.noreply.github.com>
2025-03-01 08:06:09 +0300

    Change `DLL` export; Add debug mode in `Makefile`

--- src/cpathtools.h
+++ src/cpathtools.h
@@ -5,13 +5,33 @@ extern C
 {
 #endif
 
-#ifdef CPATHTOOLSEXPORT
-#define __EXPORT(T) __declspec(dllexport) T
+// https://gcc.gnu.org/wiki/Visibility
+#if defined _WIN32 || defined __CYGWIN__
+  #ifdef BUILDING_DLL
+    #ifdef __GNUC__
+      #define DLL_PUBLIC(T) __attribute__ ((dllexport)) T
+    #else
+      #define DLL_PUBLIC(T) __declspec(dllexport) T // Note: actually gcc seems to also supports this syntax.
+    #endif
+  #else
+    #ifdef __GNUC__
+      #define DLL_PUBLIC(T) __attribute__ ((dllimport)) T
+    #else
+      #define DLL_PUBLIC(T) __declspec(dllimport) T // Note: actually gcc seems to also supports this syntax.
+    #endif
+  #endif
+  #define DLL_LOCAL(T) T
 #else
-#define __EXPORT(T) __declspec(dllimport) T
+  #if __GNUC__ >= 4
+    #define DLL_PUBLIC(T) __attribute__ ((visibility ("default"))) T
+    #define DLL_LOCAL(T)  __attribute__ ((visibility ("hidden"))) T
+  #else
+    #define DLL_PUBLIC(T) T
+    #define DLL_LOCAL(T) T
+  #endif
 #endif
 
-__EXPORT(char *) GetPath(char *__path, long long __depth);
+DLL_PUBLIC(char *) GetPath(char *__path, long long __depth);
 
 #ifdef __cplusplus
 }
